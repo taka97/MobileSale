6f92ccb24fd41b5dcc4d921cb5b2beefb62a3e4c
dang khoi <dangkhoi.tinhoc1@gmail.com>
2019-12-28 23:41:11 +0700

    Attach redux to cart. Add features: change number of product, delete product

--- src/reducers/cart.js
+++ src/reducers/cart.js
@@ -12,6 +12,24 @@ export default function getProducts(state = initState, action) {
         list: state.list.concat(action.payload)
       };
     }
+    case "ADJUST": {
+      let { list } = state;
+      let newList = list.splice(0);
+      newList[action.payload.index].number = action.payload.number;
+      return {
+        ...state,
+        list: newList
+      };
+    }
+    case "REMOVE": {
+      let { list } = state;
+      let newList = list.splice(0);
+      newList.splice(action.payload, 1);
+      return {
+        ...state,
+        list: newList
+      };
+    }
     default:
       return state;
   }
